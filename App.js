48316a2fe1fab4510cd94fc593b0674bd100e512
Uzair Iqbal <uzair.qq@outlook.com>
2022-05-16 17:47:02 +0500

    Used Logging

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  console.log("App Execeuted Loggin");
+  console.warn("App Execeuted Waning");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! Uzaior Oqnbal</Text>
       <StatusBar style="auto" />
     </View>
   );
