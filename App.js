3ad752d5adc210917af6ffaa34c511f3c6a7a1f0
Uzair Iqbal <uzair.qq@outlook.com>
2022-05-18 00:48:52 +0500

    code clean for button

--- App.js
+++ App.js
@@ -12,19 +12,6 @@ import {
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={2} onPress={() => console.log("Clicked")}>
-        Open up App.js to start working on your app! Uzaior Oqnbal....! Open up
-        App.js to start working on your app! Uzaior Oqnbal....! Open up App.js
-        to start working on your app! Uzaior Oqnbal....!
-      </Text>
-
-      <TouchableOpacity onPress={() => console.log("Touchable Clicked")}>
-        <Image
-          source={{ uri: "https://picsum.photos/200", height: 100, width: 100 }}
-          style={{ height: 100, width: 100 }}
-        />
-      </TouchableOpacity>
-
       <StatusBar style="auto" />
     </SafeAreaView>
   );
