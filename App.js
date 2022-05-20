236bf3ef0991da95921b95c5deaa4095ef1eff8b
Uzair Iqbal <uzair.qq@outlook.com>
2022-05-20 16:15:14 +0500

    Used Buttons

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <Button
+        title="Click me"
+        color={"orange"}
+        onPress={() => console.log("Button Tapped")}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
