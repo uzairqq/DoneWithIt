65d704e0bc54d1d2daafe3b5973acd080599f63f
Uzair Iqbal <uzair.qq@outlook.com>
2022-05-21 09:46:15 +0500

    Button With Alert Component

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
+import { StyleSheet, SafeAreaView, Button, Alert } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <Button title="Click Me 1" onPress={() => alert("Button 1 Tapped")} />
       <Button
-        title="Click me"
-        color={"orange"}
-        onPress={() => console.log("Button Tapped")}
+        title="Click Me 2"
+        onPress={() =>
+          Alert.alert("Button Tapped 2", "You Clicked Button 2", [
+            { text: "Yes", onPress: () => console.log("Yes Clicked") },
+            { text: "No", onPress: () => console.log("No Clicked") },
+          ])
+        }
+      />
+      <Button
+        title="Click me 3"
+        onPress={() =>
+          Alert.prompt("Button 3", "You Clicked button 3", (text) =>
+            console.log(text)
+          )
+        }
       />
       <StatusBar style="auto" />
     </SafeAreaView>
