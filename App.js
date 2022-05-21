3006ca33d723f5120180332a52b74010822f6475
Uzair Iqbal <uzair.qq@outlook.com>
2022-05-21 10:01:26 +0500

    Used Styles

--- App.js
+++ App.js
@@ -3,25 +3,8 @@ import { StyleSheet, SafeAreaView, Button, Alert } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.buttonStyle}>
       <Button title="Click Me 1" onPress={() => alert("Button 1 Tapped")} />
-      <Button
-        title="Click Me 2"
-        onPress={() =>
-          Alert.alert("Button Tapped 2", "You Clicked Button 2", [
-            { text: "Yes", onPress: () => console.log("Yes Clicked") },
-            { text: "No", onPress: () => console.log("No Clicked") },
-          ])
-        }
-      />
-      <Button
-        title="Click me 3"
-        onPress={() =>
-          Alert.prompt("Button 3", "You Clicked button 3", (text) =>
-            console.log(text)
-          )
-        }
-      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -33,5 +16,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
+  },
+  buttonStyle: {
+    backgroundColor: "red",
   },
 });
